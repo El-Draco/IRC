f100bdbf4c7e3c408ee6bc092418566da7657741
yal77 <y.alhosani12@gmail.com>
2023-10-07 13:50:10 +0400

    feat: added server port arguement to server main

--- srcs/server/main_server.cpp
+++ srcs/server/main_server.cpp
@@ -4,7 +4,15 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  server myserver(12345);
-  cout << "Created server" << endl;
+  if (argc < 2) {
+    cerr << "Error: no port number specified" << endl;
+    cerr << "Usage: irc_server [port number]" << endl;
+    exit(1);
+  }
+
+  int portNumber = atoi(argv[1]);
+  cout << "Starting server on port " << portNumber << " ..." << endl;
+
+  server myserver(portNumber);
   myserver.recieveConnections();
 }
